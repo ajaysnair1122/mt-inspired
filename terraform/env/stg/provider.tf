9bbabbd757f6eb0d83547e6605d8c1f5ec4cd982
Ajay S Nair <121013130+ajaysnair1122@users.noreply.github.com>
2026-04-30 13:47:36 +0530

    Create provider.tf

---
+++ terraform/env/stg/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  required_version = ">= 1.9.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
