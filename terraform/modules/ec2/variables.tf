3af5cceb7fb47dee6af6a48d4d61c2d7dfc03fcf
Ajay S Nair <121013130+ajaysnair1122@users.noreply.github.com>
2026-04-30 13:50:43 +0530

    Create variables.tf

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "vpc_id" {}
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
+variable "environment" {}
+variable "key_name" {}
+variable "my_ip" {}
